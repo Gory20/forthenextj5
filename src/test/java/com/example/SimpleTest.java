74116d1693bb666a47377f7703f87a6a6c03abdd
Nat @ NextWork <natasha+aws@nextwork.org>
2025-03-13 09:19:46 +1300

    Update SimpleTest.java

--- src/test/java/com/example/SimpleTest.java
+++ src/test/java/com/example/SimpleTest.java
@@ -1,24 +1,11 @@
 package com.example;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class SimpleTest {
     @Test
-    public void testSimpleAddition() {
-        assertEquals(4, 2 + 2);
-    }
-    
-    @Test
-    public void testStringComparison() {
-        String message = "Hello Natasha!";
-        assertTrue(message.contains("Natasha"));
-    }
-    
-    @Test
-    public void testApplicationName() {
-        String appName = "NextWork";
-        assertFalse(appName.isEmpty());
-        assertEquals("NextWork", appName);
+    public void simpleTest() {
+        assertTrue(true);
     }
 }
